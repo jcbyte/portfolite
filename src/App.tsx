46320c2e68a22282e0b1c7e472d69259678f4af3
jcbyte <57547887+jcbyte@users.noreply.github.com>
2024-08-26 23:00:54 +0100

    added descriptive words

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,23 @@ import myPhoto from "./images/FriendlyPhoto1MB.jpg";
 export default function App() {
 	return (
 		<>
-			<PhotoProfile name="Joel Cutler" photo={myPhoto} words={["Developer", "Student", "Partier"]} />
+			<PhotoProfile
+				name="Joel Cutler"
+				photo={myPhoto}
+				words={[
+					"Software Developer",
+					"Student",
+					"Tech Enthusiast",
+					"Passionate Coder",
+					"Self Motivated",
+					"Quick Learner",
+					"Logical Thinker",
+					"Dedicated Worker",
+					"Team Player",
+					"Problem Solver",
+					"Organised Planner",
+				]}
+			/>
 		</>
 	);
 }
