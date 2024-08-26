adea7d7a116119220348f777cb0647f6e48ac7f2
jcbyte <57547887+jcbyte@users.noreply.github.com>
2024-08-26 23:47:20 +0100

    changed styling in photo profile component

--- src/components/pageSections/PhotoProfile.tsx
+++ src/components/pageSections/PhotoProfile.tsx
@@ -12,16 +12,18 @@ export default function PhotoProfile({
 }) {
 	return (
 		<>
-			<div className="flex h-[768px]">
-				<div className="w-2/5 h-full p-2">
+			<div className="flex h-[28rem] p-4 gap-4">
+				<div className="w-2/5 h-full">
 					<img src={photo} alt="Profile Picture" className="size-full object-cover rounded-lg" />
 				</div>
 				<div className="w-3/5 h-full">
-					<div className="size-full relative flex flex-col items-center justify-center gap-1">
-						<HoverVignette bgClass="bg-dot-white/[0.4]" radius="200px" />
+					<div className="size-full relative flex flex-col items-center justify-center gap-1 rounded-lg">
+						<HoverVignette bgClass="bg-dot-white/[0.4]" radius="150px" />
 						<div className="absolute pointer-events-none inset-0 bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_50%,black)]" />
-						<div className="font-medium tracking-tight text-zinc-100 text-4xl md:text-7xl">{name}</div>
-						<div className="font-medium tracking-tight text-zinc-100 text-2xl md:text-4xl">
+						<div className="pointer-events-none font-normal tracking-tight text-zinc-100 text-4xl md:text-7xl">
+							{name}
+						</div>
+						<div className="pointer-events-none font-light tracking-tight text-zinc-100 text-2xl md:text-4xl">
 							<FlipWords words={words} duration={3000} />
 						</div>
 					</div>
