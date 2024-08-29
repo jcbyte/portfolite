4c0535b16116efae0fa3d3ff16ae512d970a286c
Joel Cutler <joelcutler108@gmail.com>
2024-08-29 20:45:10 +0100

    added linear gradient

--- src/components/pageSections/About.tsx
+++ src/components/pageSections/About.tsx
@@ -3,9 +3,10 @@
 
 export default function About() {
 	return (
-		<div className="p-4 bg-char rounded-lg h-[24rem] flex flex-col justify-center items-center gap-4 bg-zinc-800/25">
-			<div className="text-5xl">About Me</div>
-			<div className="text-xl max-w-[46rem] text-center">
+		<div className="relative p-4 bg-char rounded-lg h-[24rem] flex flex-col justify-center items-center gap-4 bg-zinc-800/25">
+			<div className="absolute pointer-events-none inset-0 bg-black [mask-image:linear-gradient(to_top_right,transparent_100%,transparent_50%)]" />
+			<div className="text-5xl font-normal">About Me</div>
+			<div className="text-xl font-light max-w-[46rem] text-center">
 				I'm a <b>passionate</b> and <b>dedicated</b> tech enthusiast and student. With a strong foundation in various
 				<b>programming languages</b>, I thrive in environments that challenge my <b>logical thinking</b> and
 				<b>problem-solving skills</b>. My interest in <b>electronics</b> and <b>theoretical physics</b> has led me to
