911002985a76aa89561dbf3eb151488b5183151a
Joel Cutler <joelcutler108@gmail.com>
2024-08-31 12:44:37 +0100

    removed photo profile props and placed them as constants in the component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,10 @@
 import "./App.css";
 import About from "./components/pageSections/About";
 import PhotoProfile from "./components/pageSections/PhotoProfile";
-import myPhoto from "./images/FriendlyPhoto1MB.jpg";
-
 export default function App() {
 	return (
 		<div className="p-4 flex flex-col gap-4">
-			<PhotoProfile
-				name="Joel Cutler"
-				photo={myPhoto}
-				words={[
-					"Software Developer",
-					"Student",
-					"Tech Enthusiast",
-					"Passionate Coder",
-					"Cat Lover",
-					"Self Motivated",
-					"Quick Learner",
-					"Logical Thinker",
-					"Dedicated Worker",
-				]}
-			/>
+			<PhotoProfile />
 			<About />
 		</div>
 	);
--- src/components/pageSections/PhotoProfile.tsx
+++ src/components/pageSections/PhotoProfile.tsx
@@ -1,20 +1,26 @@
+import myPhoto from "../../images/FriendlyPhoto1MB.jpg";
 import FlipWords from "../ui/FlipWords";
 import HoverVignette from "../ui/HoverVignette";
 
-export default function PhotoProfile({
-	name,
-	photo,
-	words,
-}: {
-	name: string;
-	photo: string;
-	words: [string, ...string[]];
-}) {
+const name = "Joel Cutler";
+const flipWords: [string, ...string[]] = [
+	"Software Developer",
+	"Student",
+	"Tech Enthusiast",
+	"Passionate Coder",
+	"Cat Lover",
+	"Self Motivated",
+	"Quick Learner",
+	"Logical Thinker",
+	"Dedicated Worker",
+];
+
+export default function PhotoProfile({}: {}) {
 	return (
 		<>
 			<div className="flex h-[28rem] gap-4">
 				<div className="w-2/5 h-full">
-					<img src={photo} alt="Profile Picture" className="size-full object-cover rounded-lg" />
+					<img src={myPhoto} alt="Profile Picture" className="size-full object-cover rounded-lg" />
 				</div>
 				<div className="w-3/5 h-full">
 					<div className="size-full relative flex flex-col items-center justify-center gap-1 rounded-lg">
@@ -22,7 +28,7 @@ export default function PhotoProfile({
 						<div className="absolute pointer-events-none inset-0 bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_50%,black)]" />
 						<div className="pointer-events-none font-normal tracking-tight text-zinc-100 text-7xl">{name}</div>
 						<div className="pointer-events-none font-light tracking-tight text-zinc-100 text-4xl">
-							<FlipWords words={words} duration={3000} />
+							<FlipWords words={flipWords} duration={3000} />
 						</div>
 					</div>
 				</div>
