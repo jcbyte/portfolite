a0c5e4fe2034c93a9459a2f90deab3f5c2d41a75
Joel Cutler <joelcutler108@gmail.com>
2024-09-01 17:42:17 +0100

    created hover animation

--- src/components/ui/FancyWord.tsx
+++ src/components/ui/FancyWord.tsx
@@ -1,17 +1,37 @@
 import { motion } from "framer-motion";
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 import Sparkles from "./Sparkles";
 
 const wordAnimation = {
-	whileHover: { scale: 1.2 },
+	initial: {},
+	animate: {
+		scale: 1.8,
+		textShadow: "0 0 10px var(--cyan-400)",
+		rotateX: 360,
+		color: "var(--cyan-500)",
+	},
 	transition: { type: "spring", stiffness: 100, duration: 0.1 },
 };
 
 export default function FancyWord({ children, id }: { children: ReactNode; id: string }) {
+	const [isHovered, setIsHovered] = useState<boolean>(false);
+
+	const { animate: wordAnimationAnimate, ...wordAnimationExtra } = wordAnimation;
+
 	return (
-		<motion.div className="relative font-bold inline-block px-1 align-baseline leading-[1em]" {...wordAnimation}>
+		<div
+			className="relative font-bold inline-block px-1 align-baseline leading-[1em]"
+			onMouseEnter={() => setIsHovered(true)}
+			onMouseLeave={() => setIsHovered(false)}
+		>
 			<Sparkles id={`fancy-${id}`} />
-			{children}
-		</motion.div>
+			<motion.div
+				className="font-bold align-baseline leading-[1em]"
+				animate={isHovered ? wordAnimationAnimate : undefined}
+				{...wordAnimationExtra}
+			>
+				{children}
+			</motion.div>
+		</div>
 	);
 }
