a9793842ffed3c597e060a09545609101150929f
jcbyte <57547887+jcbyte@users.noreply.github.com>
2024-09-15 07:43:55 +0100

    added overarching text to contact section

--- src/components/pageSections/ContactSection.tsx
+++ src/components/pageSections/ContactSection.tsx
@@ -1,5 +1,10 @@
-// TODO add email, github, instagram, etc
+// TODO add email, github, instagram, etc - logos and links
 
 export default function ContactSection() {
-	return <div className="bg-zinc-900 flex justify-center p-4">ContactSection</div>;
+	return (
+		<div className="bg-zinc-900 flex flex-col justify-center items-center p-4 gap-2">
+			<div className="text-4xl">Contact Me</div>
+			<div className="flex gap-2">email, github, instagram, etc</div>
+		</div>
+	);
 }
