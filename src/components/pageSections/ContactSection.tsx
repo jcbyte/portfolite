89ffd3d82b59919bbd9076cc4832eefe25988663
jcbyte <57547887+jcbyte@users.noreply.github.com>
2024-09-15 07:48:10 +0100

    added contact icons

--- src/components/pageSections/ContactSection.tsx
+++ src/components/pageSections/ContactSection.tsx
@@ -1,10 +1,16 @@
-// TODO add email, github, instagram, etc - logos and links
+import { IconBrandGithub, IconBrandInstagram, IconMail } from "@tabler/icons-react";
+
+// TODO links
 
 export default function ContactSection() {
 	return (
 		<div className="bg-zinc-900 flex flex-col justify-center items-center p-4 gap-2">
 			<div className="text-4xl">Contact Me</div>
-			<div className="flex gap-2">email, github, instagram, etc</div>
+			<div className="p-2 flex gap-2">
+				<IconMail></IconMail>
+				<IconBrandInstagram></IconBrandInstagram>
+				<IconBrandGithub></IconBrandGithub>
+			</div>
 		</div>
 	);
 }
