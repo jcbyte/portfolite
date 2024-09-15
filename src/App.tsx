78bd762fdbd692910432f16a5a77e5cc0eca8c0a
jcbyte <57547887+jcbyte@users.noreply.github.com>
2024-09-15 07:39:55 +0100

    added contact section element

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { useEffect } from "react";
 import { loadFull } from "tsparticles";
 import "./App.css";
 import AboutSection from "./components/pageSections/AboutSection";
+import ContactSection from "./components/pageSections/ContactSection";
 import PhotoProfileSection from "./components/pageSections/PhotoProfileSection";
 export default function App() {
 	// this should be run only once per application lifetime
@@ -16,9 +17,12 @@ export default function App() {
 	}, []);
 
 	return (
-		<div className="p-4 flex flex-col gap-4">
-			<PhotoProfileSection />
-			<AboutSection />
-		</div>
+		<>
+			<div className="p-4 flex flex-col gap-4">
+				<PhotoProfileSection />
+				<AboutSection />
+			</div>
+			<ContactSection />
+		</>
 	);
 }
---
+++ src/components/pageSections/ContactSection.tsx
@@ -0,0 +1,5 @@
+// TODO add email, github, instagram, etc
+
+export default function ContactSection() {
+	return <div className="bg-zinc-900 flex justify-center p-4">ContactSection</div>;
+}
