0765410def986281f0d776ef5ce3a0dfc7c657b0
jcbyte <57547887+jcbyte@users.noreply.github.com>
2024-09-16 11:15:10 +0100

    updated contact section to use new icon links

--- src/components/pageSections/ContactSection.tsx
+++ src/components/pageSections/ContactSection.tsx
@@ -1,22 +1,15 @@
 import { IconBrandGithub, IconBrandInstagram, IconBrandLinkedin, IconMail } from "@tabler/icons-react";
+import IconLink from "../ui/IconLink";
 
 export default function ContactSection() {
 	return (
 		<div className="bg-zinc-900 flex flex-col justify-center items-center p-4 gap-2">
 			<div className="text-4xl">Contact Me</div>
 			<div className="p-2 flex gap-2 justify-center items-center">
-				<a href="mailto:joelcutler108@gmail.com">
-					<IconMail size={30} />
-				</a>
-				<a href="https://www.instagram.com/joelcutler7" target="_blank" rel="noopener noreferrer">
-					<IconBrandInstagram size={30} />
-				</a>
-				<a href="https://github.com/jcbyte" target="_blank" rel="noopener noreferrer">
-					<IconBrandGithub size={30} />
-				</a>
-				<a href="https://www.linkedin.com/in/joelcutler/" target="_blank" rel="noopener noreferrer">
-					<IconBrandLinkedin size={30} />
-				</a>
+				<IconLink link="mailto:joelcutler108@gmail.com" Icon={IconMail} />
+				<IconLink link="https://www.instagram.com/joelcutler7/" Icon={IconBrandInstagram} />
+				<IconLink link="https://github.com/jcbyte" Icon={IconBrandGithub} />
+				<IconLink link="https://www.linkedin.com/in/joelcutler/" Icon={IconBrandLinkedin} />
 			</div>
 		</div>
 	);
