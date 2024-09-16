9c80f8c7fa796e75a9b07564a5d957b7d41121ce
jcbyte <57547887+jcbyte@users.noreply.github.com>
2024-09-16 11:21:50 +0100

    added hover effect to icon link

--- src/components/pageSections/ContactSection.tsx
+++ src/components/pageSections/ContactSection.tsx
@@ -6,10 +6,10 @@ export default function ContactSection() {
 		<div className="bg-zinc-900 flex flex-col justify-center items-center p-4 gap-2">
 			<div className="text-4xl">Contact Me</div>
 			<div className="p-2 flex gap-2 justify-center items-center">
-				<IconLink link="mailto:joelcutler108@gmail.com" Icon={IconMail} />
-				<IconLink link="https://www.instagram.com/joelcutler7/" Icon={IconBrandInstagram} />
-				<IconLink link="https://github.com/jcbyte" Icon={IconBrandGithub} />
-				<IconLink link="https://www.linkedin.com/in/joelcutler/" Icon={IconBrandLinkedin} />
+				<IconLink link="mailto:joelcutler108@gmail.com" Icon={IconMail} size={30} />
+				<IconLink link="https://www.instagram.com/joelcutler7/" Icon={IconBrandInstagram} size={30} />
+				<IconLink link="https://github.com/jcbyte" Icon={IconBrandGithub} size={30} />
+				<IconLink link="https://www.linkedin.com/in/joelcutler/" Icon={IconBrandLinkedin} size={30} />
 			</div>
 		</div>
 	);
--- src/components/ui/IconLink.tsx
+++ src/components/ui/IconLink.tsx
@@ -1,10 +1,29 @@
 import { IconProps } from "@tabler/icons-react";
+import { motion } from "framer-motion";
 import React from "react";
 
-export default function IconLink({ link, Icon }: { link: string; Icon: React.ForwardRefExoticComponent<IconProps> }) {
+export default function IconLink({
+	link,
+	Icon,
+	size,
+	colour,
+	hoverColour,
+}: {
+	link: string;
+	Icon: React.ForwardRefExoticComponent<IconProps>;
+	size?: number;
+	colour?: string;
+	hoverColour?: string;
+}) {
 	return (
-		<a href={link} target="_blank" rel="noopener noreferrer">
-			<Icon size={30} />
-		</a>
+		<motion.a
+			href={link}
+			target="_blank"
+			rel="noopener noreferrer"
+			style={{ color: `var(--${colour ?? "zinc-200"})` }}
+			whileHover={{ color: `var(--${hoverColour ?? "cyan-500"})` }} // Change to desired color on hover
+		>
+			<Icon size={size} />
+		</motion.a>
 	);
 }
