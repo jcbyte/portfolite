fef1015b8ae904a4563c4ed6f6fd182eaeca5282
jcbyte <57547887+jcbyte@users.noreply.github.com>
2024-09-23 21:57:19 +0100

    create empty space where navbar would belong on the page

--- src/components/pageSections/Navbar.tsx
+++ src/components/pageSections/Navbar.tsx
@@ -1,19 +1,16 @@
-// TODO animation from full to floating when scrolling from top.
-
 import { motion } from "framer-motion";
 import { useEffect, useState } from "react";
 
 export default function Navbar() {
-	const [isScrolled, setIsScrolled] = useState(false);
+	const [isScrolled, setIsScrolled] = useState<boolean>(false);
+	const [navbarHeight, setNavbarHeight] = useState<number>(0);
 
-	// Track scroll position to toggle navbar state
 	useEffect(() => {
 		const handleScroll = () => {
-			if (window.scrollY > 50) {
-				console.log("t");
+			console.log(window.scrollY, navbarHeight);
+			if (window.scrollY > navbarHeight) {
 				setIsScrolled(true);
 			} else {
-				console.log("f");
 				setIsScrolled(false);
 			}
 		};
@@ -23,23 +20,37 @@ export default function Navbar() {
 		return () => {
 			window.removeEventListener("scroll", handleScroll);
 		};
-	}, []);
+	}, [navbarHeight]);
+
+	const getNavbarHeight = (node: HTMLDivElement) => {
+		console.log(node);
+
+		if (node) {
+			const calculatedHeight: number = node.getBoundingClientRect().height;
+			setNavbarHeight(calculatedHeight);
+			console.log(calculatedHeight);
+		}
+	};
 
 	return (
-		// TODO create gap here which is the same height as the full navbar
+		<>
+			<div className="w-full fixed z-50">
+				<motion.div
+					ref={getNavbarHeight}
+					className="bg-zinc-800 absolute p-2 rounded-full"
+					animate={{
+						left: isScrolled ? "2rem" : 0,
+						right: isScrolled ? "2rem" : 0,
+						top: isScrolled ? "0.5rem" : 0,
+						borderRadius: isScrolled ? "8px" : 0,
+					}}
+				>
+					Navbar
+				</motion.div>
+			</div>
 
-		<div className="w-full fixed z-50">
-			<motion.div
-				className="bg-zinc-800 absolute p-2 rounded-full"
-				animate={{
-					left: isScrolled ? "2rem" : 0,
-					right: isScrolled ? "2rem" : 0,
-					top: isScrolled ? "0.5rem" : 0,
-					borderRadius: isScrolled ? "8px" : 0,
-				}}
-			>
-				Navbar
-			</motion.div>
-		</div>
+			{/* This div is to create empty space where the "static" navbar should be when the user is at the top of the page */}
+			<div style={{ height: `${navbarHeight}px` }}></div>
+		</>
 	);
 }
