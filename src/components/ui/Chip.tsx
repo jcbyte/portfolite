d22f0dd7ea56b3377787c7da3d6e9e1c3c3ef670
Joel Cutler <joelcutler108@gmail.com>
2024-09-29 18:27:12 +0100

    added chip component

---
+++ src/components/ui/Chip.tsx
@@ -0,0 +1,5 @@
+import { ReactNode } from "react";
+
+export default function Chip({ children }: { children: ReactNode }) {
+	return <div className="bg-zinc-800 rounded-full w-fit py-1 px-4 text-sm font-semibold">{children}</div>;
+}
