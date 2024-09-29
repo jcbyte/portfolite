82e48ec8629f98b318d037cd15b543b38faf1d17
Joel Cutler <joelcutler108@gmail.com>
2024-09-29 23:25:14 +0100

    added extra information and optional grade section

--- src/components/pageSections/EducationSection.tsx
+++ src/components/pageSections/EducationSection.tsx
@@ -5,37 +5,48 @@ import Chip from "../ui/Chip";
 // TODO surrounding stuff (title?)
 // TODO custom scroll bar
 
-type EducationSubject = { subject: string; grade: string };
+type EducationSubject = { subject: string; grade?: string };
 type Education = {
 	school: string;
 	location: string;
 	start: string;
 	finish: string;
-	grades: EducationSubject[];
+	subject: string;
+	grades?: EducationSubject[];
 	extra?: ReactNode;
 };
 
 const educations: Education[] = [
 	{
 		school: "University of York",
-		location: "York",
+		location: "Heslington, York",
 		start: "September 2023",
 		finish: "Current",
-		grades: [{ subject: "MEng (Hons) Computer Science", grade: "Current" }],
-		// TODO this seems weird
+		subject: "MEng Computer Science",
 	},
 	{
 		school: "Self-Study",
-		location: "Cornwall",
+		location: "Bodmin, Cornwall",
 		start: "December 2022",
 		finish: "May 2023",
+		subject: "A Level",
 		grades: [{ subject: "A Level Further Mathematics", grade: "A*" }],
+		extra: (
+			<>
+				<div>During my Gap year I self-studied A-Level Further Mathematics.</div>
+				<div className="mt-2">
+					This demanded a high level of self-motivation, clear focus and time management to ensure I could cover and
+					understand the entire course, with my work paying off resulting in another A*.
+				</div>
+			</>
+		),
 	},
 	{
 		school: "Callywith College",
 		location: "Bodmin, Cornwall",
 		start: "September 2020",
 		finish: "May 2022",
+		subject: "A Level",
 		grades: [
 			{ subject: "A Level Computer Science", grade: "A*" },
 			{ subject: "A Level Mathematics", grade: "A*" },
@@ -48,19 +59,36 @@ const educations: Education[] = [
 		location: "Launceston, Cornwall",
 		start: "September 2015",
 		finish: "May 2020",
+		subject: "GCSE",
 		grades: [
 			{ subject: "GCSE Computer Science", grade: "9" },
 			{ subject: "GCSE Mathematics", grade: "9" },
 			{ subject: "GCSE Physics", grade: "8" },
-			{ subject: "GCSE Chemistry", grade: "8" },
-			{ subject: "GCSE Biology", grade: "7" },
-			{ subject: "GCSE Design and Technology", grade: "8" },
-			{ subject: "GCSE Economics", grade: "7" },
-			{ subject: "GCSE Geography", grade: "7" },
-			{ subject: "GCSE English Language", grade: "6" },
-			{ subject: "GCSE English Literature", grade: "6" },
-			// TODO list some of these in the extra section
+			{ subject: "7 More Subjects" },
 		],
+		extra: (
+			<div className="flex flex-col gap-2">
+				<div className="text-md font-semibold">More Subjects:</div>
+
+				{[
+					{ subject: "GCSE Chemistry", grade: "8" },
+					{ subject: "GCSE Biology", grade: "7" },
+					{ subject: "GCSE Design and Technology", grade: "8" },
+					{ subject: "GCSE Economics", grade: "7" },
+					{ subject: "GCSE Geography", grade: "7" },
+					{ subject: "GCSE English Language", grade: "6" },
+					{ subject: "GCSE English Literature", grade: "6" },
+				].map((subject: EducationSubject, i) => {
+					return (
+						<Chip key={i}>
+							<div className="text-sm font-semibold">
+								{subject.subject}: <div className="inline-block font-bold">{subject.grade}</div>
+							</div>
+						</Chip>
+					);
+				})}
+			</div>
+		),
 	},
 ];
 
@@ -88,26 +116,34 @@ function EducationBlock({
 					<div className="text-sm mt-1">
 						{education.start} - {education.finish}
 					</div>
+					<div className="text-lg mt-2 font-semibold">{education.subject}</div>
 				</div>
-				<div className="flex flex-col gap-2 mt-4">
-					<div className="text-md font-semibold">Grades:</div>
-					{education.grades.map((subject: EducationSubject, i) => {
-						return (
-							<Chip key={i}>
-								<div className="text-sm font-semibold">
-									{subject.subject}: <div className="inline-block font-bold">{subject.grade}</div>
-								</div>
-							</Chip>
-						);
-					})}
-				</div>
+				{education.grades && (
+					<div className="flex flex-col gap-2 mt-4">
+						<div className="text-md font-semibold">Subjects:</div>
+						{education.grades.map((subject: EducationSubject, i) => {
+							return (
+								<Chip key={i}>
+									<div className="text-sm font-semibold">
+										{subject.subject}
+										{subject.grade && (
+											<>
+												: <div className="inline-block font-bold">{subject.grade}</div>
+											</>
+										)}
+									</div>
+								</Chip>
+							);
+						})}
+					</div>
+				)}
 
 				<motion.div
-					className="absolute left-[18rem] w-[16rem]"
+					className="absolute left-[16rem] w-[16rem]"
 					animate={{ opacity: expanded ? 1 : 0 }}
 					transition={{ duration: 0.1 }}
 				>
-					{education.extra ?? "No extra information"}
+					{education.extra ?? "Nothing else to say!"}
 				</motion.div>
 			</div>
 		</motion.div>
