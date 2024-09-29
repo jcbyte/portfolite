58ab9b2b507a4094053fff9481b40d4b298f0d2f
Joel Cutler <joelcutler108@gmail.com>
2024-09-29 19:31:53 +0100

    moved education data into constant variable

--- src/components/pageSections/EducationSection.tsx
+++ src/components/pageSections/EducationSection.tsx
@@ -1,8 +1,31 @@
 import { motion } from "framer-motion";
-import { useState } from "react";
+import { ReactNode, useState } from "react";
 import Chip from "../ui/Chip";
 
-// TODO create template for all
+// TODO list all
+
+type EducationSubject = { subject: string; grade: string };
+type Education = {
+	school: string;
+	location: string;
+	start: string;
+	finish: string;
+	grades: EducationSubject[];
+	extra?: ReactNode;
+};
+
+const education: Education = {
+	school: "Callywith College",
+	location: "Bodmin, Cornwall",
+	start: "September 2020",
+	finish: "May 2022",
+	grades: [
+		{ subject: "A Level Computer Science", grade: "A*" },
+		{ subject: "A Level Mathematics", grade: "A*" },
+		{ subject: "A Level Physics", grade: "A*" },
+		{ subject: 'EPQ "Quantum Computing"', grade: "A*" },
+	],
+};
 
 export default function EducationSection() {
 	const [isExpanded, setIsExpanded] = useState<boolean>(false);
@@ -21,32 +44,23 @@ export default function EducationSection() {
 		>
 			<div className="flex flex-col h-full">
 				<div className="flex-grow flex flex-col w-[18rem]">
-					<div className="text-xl font-bold">Callywith College</div>
-					<div className="text-sm text-zinc-400">Bodmin, Cornwall</div>
-					<div className="text-sm mt-1">September 2020 - May 2022</div>
+					<div className="text-xl font-bold">{education.school}</div>
+					<div className="text-sm text-zinc-400">{education.location}</div>
+					<div className="text-sm mt-1">
+						{education.start} - {education.finish}
+					</div>
 				</div>
 				<div className="flex flex-col gap-2 mt-4">
 					<div className="text-md font-semibold">Grades:</div>
-					<Chip>
-						<div className="text-sm font-semibold">
-							A Level Computer Science: <div className="inline-block font-bold">A*</div>
-						</div>
-					</Chip>
-					<Chip>
-						<div className="text-sm font-semibold">
-							A Level Mathematics: <div className="inline-block font-bold">A*</div>
-						</div>
-					</Chip>
-					<Chip>
-						<div className="text-sm font-semibold">
-							A Level Physics: <div className="inline-block font-bold">A*</div>
-						</div>
-					</Chip>
-					<Chip>
-						<div className="text-sm font-semibold">
-							EPQ "Quantum Computing": <div className="inline-block font-bold">A*</div>
-						</div>
-					</Chip>
+					{education.grades.map((subject: EducationSubject, i) => {
+						return (
+							<Chip key={i}>
+								<div className="text-sm font-semibold">
+									{subject.subject}: <div className="inline-block font-bold">{subject.grade}</div>
+								</div>
+							</Chip>
+						);
+					})}
 				</div>
 
 				<motion.div
@@ -54,8 +68,7 @@ export default function EducationSection() {
 					animate={{ opacity: isExpanded ? 1 : 0 }}
 					transition={{ duration: 0.1 }}
 				>
-					Write some content here please Write some content here please Write some content here please Write some
-					content here please Write some content here please Write some content here please{" "}
+					{education.extra ?? "No extra information"}
 				</motion.div>
 			</div>
 		</motion.div>
